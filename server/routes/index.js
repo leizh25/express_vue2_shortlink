1319221cc5534d6daa3e9539a24f0b73d2a72700
leizh25 <leizhuzheng@qq.com>
2023-11-04 00:47:32 +0800

    短链接接口功能实现

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,18 @@ const express = require("express")
 const router = express.Router()
 const UrlModel = require("../models/Url")
 
+// @route GET /:code
+// @desc 重定向到长链接
 router.get("/:code", async (req, res) => {
-  // ...
+  // 获取url对象
+  const url = await UrlModel.findOne({ urlCode: req.params.code })
+
+  //检测url是否存在
+  if (url) {
+    res.redirect(url.longUrl)
+  } else {
+    res.status(404).json("Server error")
+  }
 })
 
 module.exports = router
